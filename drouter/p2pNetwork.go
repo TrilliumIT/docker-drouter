a4ee74e695d7ab71090992db0071eda81e476451
Travis Hegner <thegner@trilliumit.com>
2016-06-17 17:01:38 -0400

    NOT CURRENTLY BUILDING: WIP: switching to event/data driven main loop

--- drouter/p2pNetwork.go
+++ drouter/p2pNetwork.go
@@ -5,52 +5,49 @@ import (
 	"github.com/vishvananda/netlink"
 	"github.com/ziutek/utils/netaddr"
 	"net"
+	"os"
 )
 
 type p2pNetwork struct {
-	hostLinkIndex int
-	selfLinkIndex int
-	network       *net.IPNet
-	hostIP        net.IP
-	selfIP        net.IP
+	network *net.IPNet
+	hostIP  net.IP
+	selfIP  net.IP
 }
 
-func (dr *DistributedRouter) makeP2PLink(p2paddr string) error {
+func makeP2PLink(p2paddr string) error {
 	log.Debugf("Making a p2p network for: %v", p2paddr)
 	host_link_veth := &netlink.Veth{
 		LinkAttrs: netlink.LinkAttrs{Name: "drouter_veth0"},
 		PeerName:  "drouter_veth1",
 	}
-	err := dr.hostNamespace.LinkAdd(host_link_veth)
+	err := hostNamespace.LinkAdd(host_link_veth)
 	if err != nil {
 		return err
 	}
-	host_link, err := dr.hostNamespace.LinkByName("drouter_veth0")
+	host_link, err := hostNamespace.LinkByName("drouter_veth0")
 	if err != nil {
 		return err
 	}
-	dr.p2p.hostLinkIndex = host_link.Attrs().Index
 
-	int_link, err := dr.hostNamespace.LinkByName("drouter_veth1")
+	int_link, err := hostNamespace.LinkByName("drouter_veth1")
 	if err != nil {
 		return err
 	}
-	err = dr.hostNamespace.LinkSetNsPid(int_link, dr.pid)
+	err = hostNamespace.LinkSetNsPid(int_link, os.GetPid())
 	if err != nil {
 		return err
 	}
-	int_link, err = dr.selfNamespace.LinkByName("drouter_veth1")
+	int_link, err = netlink.LinkByName("drouter_veth1")
 	if err != nil {
 		return err
 	}
-	dr.p2p.selfLinkIndex = int_link.Attrs().Index
 
 	_, p2p_net, err := net.ParseCIDR(p2paddr)
 	if err != nil {
 		log.Errorf("Failed to parse the CIDR string for p2p network: %v", p2paddr)
 		return err
 	}
-	dr.p2p.network = p2p_net
+	p2p.network = p2p_net
 
 	host_addr := *p2p_net
 	host_addr.IP = netaddr.IPAdd(host_addr.IP, 1)
@@ -58,11 +55,11 @@ func (dr *DistributedRouter) makeP2PLink(p2paddr string) error {
 		IPNet: &host_addr,
 		Label: "",
 	}
-	err = dr.hostNamespace.AddrAdd(host_link, host_netlink_addr)
+	err = hostNamespace.AddrAdd(host_link, host_netlink_addr)
 	if err != nil {
 		return err
 	}
-	dr.p2p.hostIP = host_addr.IP
+	p2p.hostIP = host_addr.IP
 
 	int_addr := *p2p_net
 	int_addr.IP = netaddr.IPAdd(int_addr.IP, 2)
@@ -70,25 +67,25 @@ func (dr *DistributedRouter) makeP2PLink(p2paddr string) error {
 		IPNet: &int_addr,
 		Label: "",
 	}
-	err = dr.selfNamespace.AddrAdd(int_link, int_netlink_addr)
+	err = netlink.AddrAdd(int_link, int_netlink_addr)
 	if err != nil {
 		return err
 	}
-	dr.p2p.selfIP = int_addr.IP
+	p2p.selfIP = int_addr.IP
 
-	err = dr.selfNamespace.LinkSetUp(int_link)
+	err = netlink.LinkSetUp(int_link)
 	if err != nil {
 		return err
 	}
 
-	err = dr.hostNamespace.LinkSetUp(host_link)
+	err = hostNamespace.LinkSetUp(host_link)
 	if err != nil {
 		return err
 	}
 
 	//discover host underlay address/network
 	hunderlay := &net.IPNet{}
-	hroutes, err := dr.hostNamespace.RouteList(nil, netlink.FAMILY_V4)
+	hroutes, err := hostNamespace.RouteList(nil, netlink.FAMILY_V4)
 	if err != nil {
 		return err
 	}
@@ -97,11 +94,11 @@ Hroutes:
 		if r.Gw != nil {
 			continue
 		}
-		link, err := dr.hostNamespace.LinkByIndex(r.LinkIndex)
+		link, err := hostNamespace.LinkByIndex(r.LinkIndex)
 		if err != nil {
 			return err
 		}
-		addrs, err := dr.hostNamespace.AddrList(link, netlink.FAMILY_V4)
+		addrs, err := hostNamespace.AddrList(link, netlink.FAMILY_V4)
 		if err != nil {
 			return err
 		}
@@ -117,8 +114,8 @@ Hroutes:
 
 	log.Debugf("Discovered host underlay as: %v", hunderlay)
 
-	dr.staticRoutes = append(dr.staticRoutes, NetworkID(hunderlay))
-	dr.hostUnderlay = hunderlay
+	staticRoutes = append(staticRoutes, NetworkID(hunderlay))
+	hostUnderlay = hunderlay
 
 	hroute := &netlink.Route{
 		LinkIndex: int_link.Attrs().Index,
@@ -127,12 +124,12 @@ Hroutes:
 	}
 
 	log.Debug("Adding drouter route to %v via %v.", hroute.Dst, hroute.Gw)
-	err = dr.selfNamespace.RouteAdd(hroute)
+	err = netlink.RouteAdd(hroute)
 	if err != nil {
 		return err
 	}
 
-	for _, sr := range dr.staticRoutes {
+	for _, sr := range staticRoutes {
 		if hroute.Dst.Contains(sr.IP) {
 			srlen, srbits := sr.Mask.Size()
 			hrlen, hrbits := hroute.Dst.Mask.Size()
@@ -149,30 +146,21 @@ Hroutes:
 		}
 
 		log.Infof("Adding host route to %v via %v.", sroute.Dst, sroute.Gw)
-		err = dr.hostNamespace.RouteAdd(sroute)
+		err = hostNamespace.RouteAdd(sroute)
 		if err != nil {
 			log.Error(err)
 			continue
 		}
 	}
 
-	if dr.localGateway {
-		dr.defaultRoute = host_addr.IP
-		err = dr.setDefaultRoute()
-		if err != nil {
-			log.Error("--local-gateway=true and unable to set default route to host's p2p address.")
-			return err
-		}
-	}
-
 	return nil
 }
 
-func (dr *DistributedRouter) removeP2PLink() error {
-	host_link, err := dr.hostNamespace.LinkByName("drouter_veth0")
+func removeP2PLink() error {
+	host_link, err := hostNamespace.LinkByName("drouter_veth0")
 	if err != nil {
 		return err
 	}
 
-	return dr.hostNamespace.LinkDel(host_link)
+	return hostNamespace.LinkDel(host_link)
 }
