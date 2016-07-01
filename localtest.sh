ac07dabdea8f633879917d23c492ef95b1b82e67
Travis Hegner <thegner@trilliumit.com>
2016-07-01 15:47:50 -0400

    fixed cover.out

--- localtest.sh
+++ localtest.sh
@@ -1,16 +1,16 @@
 BASEDIR=$(realpath $(dirname "$0"))
-if [ -e $BASEDIR/coverage/cover.html ]; then
-	sudo rm -f $BASEDIR/coverage/cover.html
+if [ -e $BASEDIR/coverage/cover.out ]; then
+	sudo rm -f $BASEDIR/coverage/cover.out
 fi
 docker images alpine | grep alpine > /dev/null || docker pull alpine
 docker build -t droutertest -f $BASEDIR/Dockertest $BASEDIR
 echo "$@"
-docker run -it --name=drntest_drouter --privileged --rm -e TERM=xterm --pid=host -v /var/run/docker.sock:/var/run/docker.sock -v $BASEDIR/coverage:/coverage droutertest "go test github.com/TrilliumIT/docker-drouter/drouter -coverprofile=/coverage/cover.html $@"
+docker run -it --name=drntest_drouter --privileged --rm -e TERM=xterm --pid=host -v /var/run/docker.sock:/var/run/docker.sock -v $BASEDIR/coverage:/coverage droutertest "go test github.com/TrilliumIT/docker-drouter/drouter -coverprofile=/coverage/cover.out $@"
 ec=$?
 [ -z $(docker images -q -f dangling=true) ] || docker rmi $(docker images -q -f dangling=true)
-if [ -e $BASEDIR/coverage/cover.html ]; then
+if [ -e $BASEDIR/coverage/cover.out ]; then
 	echo "Press enter to view coverage"
 	read
-	go tool cover -html=$BASEDIR/coverage/cover.html
+	go tool cover -html=$BASEDIR/coverage/cover.out
 fi
 exit $ec
