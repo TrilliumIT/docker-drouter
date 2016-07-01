8403e5b890e6271698d38e884c813c0532c04545
Travis Hegner <thegner@trilliumit.com>
2016-07-01 15:24:51 -0400

    separate the simulation into callbacks

--- drouter/distributedrouter_test.go
+++ drouter/distributedrouter_test.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"strings"
 	"testing"
-	"time"
 
 	"github.com/TrilliumIT/iputil"
 	dockerclient "github.com/docker/engine-api/client"
@@ -66,7 +65,7 @@ func TestMain(m *testing.M) {
 func defaultOpts() *DistributedRouterOptions {
 	return &DistributedRouterOptions{
 		IPOffset:         0,
-		Aggressive:       false,
+		Aggressive:       true,
 		HostShortcut:     false,
 		ContainerGateway: false,
 		HostGateway:      false,
@@ -78,21 +77,66 @@ func defaultOpts() *DistributedRouterOptions {
 	}
 }
 
-func TestBasicAggressive(t *testing.T) {
-	opts := defaultOpts()
-	opts.Aggressive = true
+func TestDefault(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+	hook := logtest.NewGlobal()
 
-	require.Nil(t, cleanup(), "Failed to clean up.")
-	runScenarioV4(opts, t)
-	require.Nil(t, cleanup(), "Failed to clean up.")
+	opts := defaultOpts()
+
+	st := simulation{
+		opts:    opts,
+		assert:  assert,
+		require: require,
+		cb:      make(map[int]func()),
+	}
+
+	st.cb[assertInit] = func() {
+		checkLogs(hook, assert)
+
+		drn, ok := st.dr.getNetwork(st.n[0].ID)
+		assert.True(ok, "should have learned n0 by now.")
+
+		drn, ok = st.dr.getNetwork(st.n[2].ID)
+		assert.True(ok, "should have learned n2 by now.")
+		assert.True(drn.isConnected(), "drouter should be connected to n2.")
+	}
+
+	require.NoError(st.runV4(), "Scenario failed to run.")
 }
 
-func TestBasicNonAggressive(t *testing.T) {
-	opts := defaultOpts()
+func TestNoAggressive(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+	hook := logtest.NewGlobal()
 
-	require.Nil(t, cleanup(), "Failed to clean up.")
-	runScenarioV4(opts, t)
-	require.Nil(t, cleanup(), "Failed to clean up.")
+	opts := defaultOpts()
+	opts.Aggressive = false
+
+	st := simulation{
+		opts:    opts,
+		assert:  assert,
+		require: require,
+		cb:      make(map[int]func()),
+	}
+
+	st.cb[assertInit] = func() {
+		warns := 0
+		for _, e := range hook.Entries {
+			if e.Level == log.WarnLevel {
+				warns++
+			}
+			assert.True(e.Level >= log.WarnLevel, "All logs should be >= Warn, but observed log: ", e)
+		}
+		hook.Reset()
+		assert.Equal(warns, 1, "Should have recieved one warning message for running in Aggressive with no tranist net")
+
+		if drn, ok := st.dr.getNetwork(st.n[2].ID); ok {
+			assert.False(drn.isConnected(), "drouter should not be connected to n2.")
+		}
+	}
+
+	require.NoError(st.runV4(), "Scenario failed to run.")
 }
 
 func cleanup() error {
@@ -164,175 +208,17 @@ func resetGlobals() {
 	bg = context.Background()
 }
 
-func checkLogs(entries []*log.Entry, t *testing.T) {
-	for _, e := range entries {
-		assert.True(t, e.Level >= log.InfoLevel, "All logs should be >= Info, but observed log: ", e)
+func checkLogs(hook *logtest.Hook, assert *assert.Assertions) {
+	for _, e := range hook.Entries {
+		assert.True(e.Level >= log.InfoLevel, "All logs should be >= Info, but observed log: ", e)
 	}
+
+	hook.Reset()
 }
 
-// A most basic test to make sure it doesn't die on start
-func testRunClose(t *testing.T) {
-	opts := &DistributedRouterOptions{
-		Aggressive: true,
-	}
-
-	quit := make(chan struct{})
-	ech := make(chan error)
-	go func() {
-		ech <- Run(opts, quit)
-	}()
-
-	timeout := time.NewTimer(10 * time.Second)
-	var err error
-	select {
-	case <-timeout.C:
-		close(quit)
-		err = <-ech
-	case err = <-ech:
-	}
-
-	if err != nil {
-		t.Errorf("Error on Run Return: %v", err)
-	}
-}
-
-func runScenarioV4(opts *DistributedRouterOptions, t *testing.T) {
-	defer resetGlobals()
-	var err error
-
-	//Temporary debug level to write this function
-	log.SetLevel(log.DebugLevel)
-
-	assert := assert.New(t)
-	require := require.New(t)
-	hook := logtest.NewGlobal()
-	defer hook.Reset()
-
-	fmt.Println("Creating networks 0-2.")
-	n0r := createNetwork(0, false, t)
-	defer removeNetwork(n0r.ID, t)
-	n1r := createNetwork(1, true, t)
-	defer removeNetwork(n1r.ID, t)
-	n2r := createNetwork(2, true, t)
-	defer removeNetwork(n2r.ID, t)
-
-	fmt.Println("Creating containers 0-1.")
-	c0 := createContainer("0", n0r.Name, t)
-	defer removeContainer(c0, t)
-
-	c0c, err := newContainerFromID(c0)
-	assert.NoError(err, "Failed to get container object for c0.")
-	c0routes, err := c0c.handle.RouteList(nil, netlink.FAMILY_ALL)
-	assert.NoError(err, "Failed to get c0 route list.")
-
-	c1 := createContainer("1", n1r.Name, t)
-	defer removeContainer(c1, t)
-
-	quit := make(chan struct{})
-	stopChan = quit
-
-	dr, err := newDistributedRouter(opts)
-	require.NoError(err, "Failed to create dr object.")
-
-	ech := make(chan error)
-	go func() {
-		fmt.Println("Starting DRouter.")
-		ech <- dr.start()
-	}()
-
-	startDelay := time.NewTimer(20 * time.Second)
-	select {
-	case <-startDelay.C:
-		err = nil
-	case err = <-ech:
-	}
-	fmt.Println("DRouter started.")
-
-	require.NoError(err, "Run() returned an error.")
-	if !aggressive && transitNetName == "" {
-		warns := 0
-		for _, e := range hook.Entries {
-			if e.Level == log.WarnLevel {
-				warns++
-			}
-			assert.True(e.Level >= log.WarnLevel, "All logs should be >= Warn, but observed log: ", e)
-		}
-		hook.Reset()
-		assert.Equal(warns, 1, "Should have recieved one warning message for running in Aggressive with no tranist net")
-	} else {
-		checkLogs(hook.Entries, t)
-	}
-
-	drn, ok := dr.getNetwork(n1r.ID)
-	assert.True(ok, "should have learned n1 by now.")
-	assert.True(drn.isConnected(), "drouter should be connected to n1.")
-
-	drn, ok = dr.getNetwork(n0r.ID)
-	if aggressive {
-		assert.True(ok, "should have learned n0 by now.")
-	}
-	// it's okay if we learned it in non-aggressive mode, but we should only be connected if we're in aggressive mode
-	if ok {
-		assert.False(drn.isConnected(), "drouter should not be connected to n0.")
-	}
-
-	drn, ok = dr.getNetwork(n2r.ID)
-	if aggressive {
-		assert.True(ok, "should have learned n2 by now.")
-	}
-	if ok {
-		assert.Equal(aggressive, drn.isConnected(), "drouter should be connected to n2 if in aggressive mode.")
-	}
-
-	c0newRoutes, err := c0c.handle.RouteList(nil, netlink.FAMILY_ALL)
-	assert.NoError(err, "Failed to get new c0 route list.")
-	assert.EqualValues(c0routes, c0newRoutes, "c0 routes should not be modified.")
-
-	c1c, err := newContainerFromID(c1)
-	assert.NoError(err, "Failed to get container object for c1.")
-
-	assert.Equal(aggressive, handleContainsRoute(c1c.handle, testNets[2], nil, t), "c1 should have a route to n2 if in aggressive mode.")
-
-	c2 := createContainer("2", n2r.Name, t)
-	time.Sleep(5 * time.Second)
-
-	checkLogs(hook.Entries, t)
-
-	assert.False(handleContainsRoute(c1c.handle, testNets[0], nil, t), "c1 should not have a route to n0.")
-	assert.True(handleContainsRoute(c1c.handle, testNets[2], nil, t), "c1 should have a route to n2.")
-
-	c2c, err := newContainerFromID(c2)
-	assert.NoError(err, "Failed to create c2.")
-
-	assert.False(handleContainsRoute(c2c.handle, testNets[0], nil, t), "c2 should not have a route to n0.")
-	assert.True(handleContainsRoute(c2c.handle, testNets[1], nil, t), "c2 should have a route to n1.")
-
-	n3r := createNetwork(3, true, t)
-	defer removeNetwork(n3r.ID, t)
-
-	//sleep to give aggressive time to connect to those networks
-	time.Sleep(10 * time.Second)
-	assert.Equal(aggressive, handleContainsRoute(c1c.handle, testNets[3], nil, t), "c1 should have a route to n3 if in aggressive.")
-	assert.Equal(aggressive, handleContainsRoute(c2c.handle, testNets[3], nil, t), "c2 should have a route to n3 if in aggressive.")
-
-	// purposefully remove c2 and make sure c1 looses the route in non-aggressive
-	removeContainer(c2, t)
-	time.Sleep(5 * time.Second)
-	checkLogs(hook.Entries, t)
-
-	assert.Equal(aggressive, handleContainsRoute(c1c.handle, testNets[2], nil, t), "c1 should have a route to n2 in aggressive mode.")
-
-	close(quit)
-
-	assert.NoError(<-ech, "Error during drouter shutdown.")
-	checkLogs(hook.Entries, t)
-}
-
-func handleContainsRoute(h *netlink.Handle, to *net.IPNet, via *net.IP, t *testing.T) bool {
-	require := require.New(t)
-
+func handleContainsRoute(h *netlink.Handle, to *net.IPNet, via *net.IP, assert *assert.Assertions) bool {
 	routes, err := h.RouteList(nil, netlink.FAMILY_ALL)
-	require.NoError(err, "Failed to get routes from handle.")
+	assert.NoError(err, "Failed to get routes from handle.")
 
 	for _, r := range routes {
 		if r.Dst == nil || r.Gw == nil {
--- drouter/network_test.go
+++ drouter/network_test.go
@@ -21,7 +21,7 @@ const (
 	NetIPNet = "192.168.242.%v/29"
 )
 
-func createNetwork(n int, dr bool, t *testing.T) dockerTypes.NetworkResource {
+func createNetwork(n int, dr bool) (*dockerTypes.NetworkResource, error) {
 	name := fmt.Sprintf(NetName, n)
 	opts := make(map[string]string)
 	if dr {
@@ -38,145 +38,143 @@ func createNetwork(n int, dr bool, t *testing.T) dockerTypes.NetworkResource {
 			},
 		},
 	})
-	require.Equal(t, err, nil, "Error creating network")
+	if err != nil {
+		return nil, err
+	}
 
 	nr, err := dc.NetworkInspect(bg, r.ID)
-	require.Equal(t, err, nil, "Error inspecting network")
-	return nr
-}
-
-func removeNetwork(id string, t *testing.T) {
-	err := dc.NetworkRemove(bg, id)
 	if err != nil {
-		t.Fatalf("Error removing network: %v", err)
+		return nil, err
 	}
+
+	return &nr, nil
 }
 
 func TestNetworkConnect(t *testing.T) {
-	//assert := assert.New(t)
+	assert := assert.New(t)
+	require := require.New(t)
 
-	n0r := createNetwork(0, true, t)
-	defer removeNetwork(n0r.ID, t)
+	n0r, err := createNetwork(0, true)
+	require.NoError(err, "Failed to create n0.")
+	defer require.NoError(dc.NetworkRemove(bg, n0r.ID), "Failed to remove n0.")
 
 	hook := logtest.NewGlobal()
 	defer hook.Reset()
-	n0 := newNetwork(&n0r)
+	n0 := newNetwork(n0r)
 
 	n0.connect()
-	checkLogs(hook.Entries, t)
+	checkLogs(hook, assert)
 
 	n0.disconnect()
-	checkLogs(hook.Entries, t)
+	checkLogs(hook, assert)
 }
 
 func TestIPOffset(t *testing.T) {
-	defer resetGlobals()
-	assert := assert.New(t)
-	ipOffset = 2
-
-	n0r := createNetwork(0, true, t)
-	defer removeNetwork(n0r.ID, t)
-
-	hook := logtest.NewGlobal()
-	defer hook.Reset()
-	n0 := newNetwork(&n0r)
-	n0.connect()
-
-	routes, err := netlink.RouteGet(net.ParseIP("192.168.242.1"))
-	assert.Equal(err, nil, "Error getting routes")
-	assert.True(routes[0].Src.Equal(net.ParseIP("192.168.242.2")), "IP not what was expected")
-
-	n0.disconnect()
-	checkLogs(hook.Entries, t)
+	testIPOffset(2, "192.168.242.2", t)
 }
 
 func TestNegativeIPOffset(t *testing.T) {
-	defer resetGlobals()
-	assert := assert.New(t)
-	ipOffset = -1
-
-	n0r := createNetwork(0, true, t)
-	defer removeNetwork(n0r.ID, t)
-
-	hook := logtest.NewGlobal()
-	defer hook.Reset()
-	n0 := newNetwork(&n0r)
-	n0.connect()
-
-	routes, err := netlink.RouteGet(net.ParseIP("192.168.242.1"))
-	assert.Equal(err, nil, "Error getting routes")
-	assert.True(routes[0].Src.Equal(net.ParseIP("192.168.242.6")), "IP not what was expected")
-
-	n0.disconnect()
-	checkLogs(hook.Entries, t)
+	testIPOffset(-1, "192.168.242.6", t)
 }
 
-func TestMultipleConnectWarn(t *testing.T) {
-	n0r := createNetwork(0, true, t)
-	defer removeNetwork(n0r.ID, t)
+func testIPOffset(ipo int, exp string, t *testing.T) {
+	defer resetGlobals()
+	assert := assert.New(t)
+	require := require.New(t)
+	ipOffset = ipo
+
+	n0r, err := createNetwork(0, true)
+	require.NoError(err, "Failed to create n0.")
+	defer require.NoError(dc.NetworkRemove(bg, n0r.ID), "Failed to remove n0.")
 
 	hook := logtest.NewGlobal()
 	defer hook.Reset()
-	n0 := newNetwork(&n0r)
+	n0 := newNetwork(n0r)
 	n0.connect()
 	defer n0.disconnect()
-	checkLogs(hook.Entries, t)
+
+	routes, err := netlink.RouteGet(net.ParseIP("192.168.242.1"))
+	require.NoError(err, "Error getting routes")
+	assert.True(routes[0].Src.Equal(net.ParseIP(exp)), "IP not what was expected")
+
+	checkLogs(hook, assert)
+}
+
+func TestMultipleConnect(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	n0r, err := createNetwork(0, true)
+	require.NoError(err, "Failed to create n0.")
+	defer require.NoError(dc.NetworkRemove(bg, n0r.ID), "Failed to remove n0.")
+
+	hook := logtest.NewGlobal()
+	defer hook.Reset()
+	n0 := newNetwork(n0r)
+	n0.connect()
+	defer n0.disconnect()
+	checkLogs(hook, assert)
 
 	n0.connect()
-	// we should not get warnings anymore
-	checkLogs(hook.Entries, t)
+
+	checkLogs(hook, assert)
 }
 
 func TestIsConnected(t *testing.T) {
 	assert := assert.New(t)
+	require := require.New(t)
 
-	n0r := createNetwork(0, true, t)
-	defer removeNetwork(n0r.ID, t)
+	n0r, err := createNetwork(0, true)
+	require.NoError(err, "Failed to create n0.")
+	defer require.NoError(dc.NetworkRemove(bg, n0r.ID), "Failed to remove n0.")
 
-	n0 := newNetwork(&n0r)
 	hook := logtest.NewGlobal()
+	n0 := newNetwork(n0r)
 	defer hook.Reset()
 	assert.False(n0.isConnected(), "Network should not be connected")
-	checkLogs(hook.Entries, t)
+	checkLogs(hook, assert)
 
 	n0.connect()
-	hook.Reset()
 	assert.True(n0.isConnected(), "Network should be connected")
-	checkLogs(hook.Entries, t)
+	checkLogs(hook, assert)
 
 	n0.disconnect()
 	assert.False(n0.isConnected(), "Network should not be connected")
-	checkLogs(hook.Entries, t)
+	checkLogs(hook, assert)
 }
 
 func TestIsDrouterTrue(t *testing.T) {
-	assert := assert.New(t)
-
-	n0r := createNetwork(0, true, t)
-	defer removeNetwork(n0r.ID, t)
-	n0 := newNetwork(&n0r)
-
-	assert.True(n0.isDRouter())
+	testIsDrouter(true, t)
 }
 
 func TestIsDrouterFalse(t *testing.T) {
+	testIsDrouter(false, t)
+}
+
+func testIsDrouter(flag bool, t *testing.T) {
 	assert := assert.New(t)
+	require := require.New(t)
 
-	n0r := createNetwork(0, false, t)
-	defer removeNetwork(n0r.ID, t)
-	n0 := newNetwork(&n0r)
+	n0r, err := createNetwork(0, flag)
+	require.NoError(err, "Failed to create n0.")
+	defer require.NoError(dc.NetworkRemove(bg, n0r.ID), "Failed to remove n0.")
 
-	assert.False(n0.isDRouter())
+	n0 := newNetwork(n0r)
+
+	assert.Equal(flag, n0.isDRouter(), "IsDrouter returns the wrong thing.")
 }
 
 func TestIsDrouterTransit(t *testing.T) {
 	defer resetGlobals()
 	assert := assert.New(t)
+	require := require.New(t)
 
-	n0r := createNetwork(0, true, t)
-	defer removeNetwork(n0r.ID, t)
+	n0r, err := createNetwork(0, false)
+	require.NoError(err, "Failed to create n0.")
+	defer require.NoError(dc.NetworkRemove(bg, n0r.ID), "Failed to remove n0.")
+
 	transitNetID = n0r.ID
-	n0 := newNetwork(&n0r)
+	n0 := newNetwork(n0r)
 
 	assert.True(n0.isDRouter())
 }
@@ -184,97 +182,38 @@ func TestIsDrouterTransit(t *testing.T) {
 func TestAdminDownNonAggressive(t *testing.T) {
 	defer resetGlobals()
 	assert := assert.New(t)
+	require := require.New(t)
 
-	n0r := createNetwork(0, true, t)
-	defer removeNetwork(n0r.ID, t)
+	n0r, err := createNetwork(0, true)
+	require.NoError(err, "Failed to create n0.")
+	defer require.NoError(dc.NetworkRemove(bg, n0r.ID), "Failed to remove n0.")
+
 	transitNetID = n0r.ID
 	aggressive = false
-	n0 := newNetwork(&n0r)
+	n0 := newNetwork(n0r)
 
 	assert.False(n0.adminDown)
-	err := n0.disconnectEvent()
-	assert.Equal(err, nil, "Error with disconnectEvent")
+	assert.NoError(n0.disconnectEvent(), "Error with disconnectEvent")
 	assert.False(n0.adminDown, "adminDown should be False")
 }
 
 func TestAdminDownAggressive(t *testing.T) {
 	defer resetGlobals()
 	assert := assert.New(t)
-	aggressive = true
+	require := require.New(t)
 
-	n0r := createNetwork(0, true, t)
-	defer removeNetwork(n0r.ID, t)
+	n0r, err := createNetwork(0, true)
+	require.NoError(err, "Failed to create n0.")
+	defer require.NoError(dc.NetworkRemove(bg, n0r.ID), "Failed to remove n0.")
+
 	transitNetID = n0r.ID
-	n0 := newNetwork(&n0r)
+	aggressive = true
+	n0 := newNetwork(n0r)
 
 	assert.False(n0.adminDown)
-	err := n0.disconnectEvent()
-	assert.Equal(err, nil, "Error with disconnectEvent")
+	assert.NoError(n0.disconnectEvent(), "Error with disconnectEvent")
 	assert.True(n0.adminDown, "adminDown should be True")
 
-	err = n0.connectEvent()
-	assert.Equal(err, nil, "Error with connectEvent")
+	assert.NoError(n0.connectEvent(), "Error with connectEvent")
 	assert.False(n0.adminDown, "adminDown should be False")
-}
-
-func createMultiSubnetNetwork(n int, dr bool, t *testing.T) dockerTypes.NetworkResource {
-	name := fmt.Sprintf(NetName, n)
-	opts := make(map[string]string)
-	if dr {
-		opts["drouter"] = DrInst
-	}
-	r, err := dc.NetworkCreate(bg, name, dockerTypes.NetworkCreate{
-		Options: opts,
-		IPAM: dockerNTypes.IPAM{
-			Config: []dockerNTypes.IPAMConfig{
-				{
-					Subnet:  fmt.Sprintf(NetIPNet, n*8),
-					Gateway: fmt.Sprintf(NetGw, n*8+1),
-				},
-				{
-					Subnet:  fmt.Sprintf("192.168.243.%v/29", n*8),
-					Gateway: fmt.Sprintf("192.168.243.%v", n*8+1),
-				},
-			},
-		},
-	})
-	require.Equal(t, err, nil, "Error creating network")
-	nr, err := dc.NetworkInspect(bg, r.ID)
-	require.Equal(t, err, nil, "Error inspecting network")
-	return nr
-}
-
-// disabled becasue bridge driver doesn't support multiple subnets
-func testMultiSubnetIPOffset(t *testing.T) {
-	defer resetGlobals()
-	assert := assert.New(t)
-	ipOffset = 2
-
-	n0r := createMultiSubnetNetwork(0, true, t)
-	defer removeNetwork(n0r.ID, t)
-
-	hook := logtest.NewGlobal()
-	defer hook.Reset()
-	n0 := newNetwork(&n0r)
-	n0.connect()
-
-	routes, err := netlink.RouteGet(net.ParseIP("192.168.242.1"))
-	assert.Equal(err, nil, "Error getting routes")
-
-	ip1 := false
-	ip2 := false
-	for _, r := range routes {
-		if r.Src.Equal(net.ParseIP("192.168.242.2")) {
-			ip1 = true
-		}
-		if r.Src.Equal(net.ParseIP("192.168.243.2")) {
-			ip2 = true
-		}
-	}
-
-	assert.True(ip1, "First ip not set")
-	assert.True(ip2, "Second ip not set")
-
-	n0.disconnect()
-	checkLogs(hook.Entries, t)
 }
